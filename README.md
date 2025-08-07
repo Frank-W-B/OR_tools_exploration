f85f7e9cce6f4d35a07886fec4244f29ac0fffee
Frank-W-B <frank.burkholder@gmail.com>
2025-08-07 10:44:31 -0600

    add linear example

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # OR_tools_exploration
-Repository for playing with Google's OR Tools packages 
-Does this work?
+Repository for playing with Google's OR Tools packages
+
+[Package install guide](https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/)
+
+## Activating the environment  
+Install packages after the environment is activated.  
+```
+source .venv/bin/activate  
+which python  
+.venv/bin/python
+```
+## Install OR Tools
+`python -m pip install ortools`
+
+[Get started with OR-Tools for Python](https://developers.google.com/optimization/introduction/python)
+
+
+## Deactivating the environment  
+`deactivate`  
+
---
+++ simple_linear_optimization.py
@@ -0,0 +1,53 @@
+# Maximize 3x + y subject to the following constraints:
+# 0 <= x <= 1
+# 0 <= y <= 2
+# x + y <= 2
+
+import sys
+
+from ortools.init.python import init
+from ortools.linear_solver import pywraplp
+
+# Create the linear solver with GLOP backend
+solver = pywraplp.Solver.CreateSolver("GLOP")
+if not solver:
+    print("Could not create solver GLOP")
+    #return
+    sys.exit()
+
+# Create the variables X and Y
+x_var = solver.NumVar(0, 1, "x")
+y_var = solver.NumVar(0, 2, "y")
+
+print("Number of variables =", solver.NumVariables())
+
+infinity = solver.infinity()
+
+# Create a linear constraint, x + y <= 2
+constraint = solver.Constraint(-infinity, 2, "ct")
+constraint.SetCoefficient(x_var, 1)
+constraint.SetCoefficient(y_var, 1)
+
+print("Number of constraints =", solver.NumConstraints())
+
+# Create the objective function: 3 * x + y
+objective = solver.Objective()
+objective.SetCoefficient(x_var, 3)
+objective.SetCoefficient(y_var, 1)
+objective.SetMaximization()
+
+print(f"Solving with {solver.SolverVersion()}")
+result_status = solver.Solve()
+print(f"Status: {result_status}")
+if result_status != pywraplp.Solver.OPTIMAL:
+    print("The problem does not have an optimal solution.")
+    if result_status == pywraplp.Solver.FEASIBLE:
+        print("A potentially suboptimal solution was found.")
+    else:
+        print("The solver could not solve the problem.")
+        sys.exit()
+
+print("Solution:")
+print("Objective value =", objective.Value())
+print("x =", x_var.solution_value())
+print("y =", y_var.solution_value())
